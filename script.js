3063b7ac334b778afce9f168949a3a05dbc29677
Emma Franzén <emmasmejladress@gmail.com>
2026-02-20 09:29:10 +0100

    Change trivia category in API URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const API_URL = "https://opentdb.com/api.php?amount=5&category=13&difficulty=easy&type=multiple";
+  const API_URL = "https://opentdb.com/api.php?amount=5&category=14&difficulty=easy&type=multiple";
   const questionBox = document.getElementById("question");
   const optionsBox = document.getElementById("options");
   const result = document.getElementById("result");
