8ea88aedb37b21db737cb4b2a9c41f63e2d30073
Emma Franzén <emmasmejladress@gmail.com>
2026-03-17 15:19:44 +0100

    Show message if not Friday for quiz questions

--- script.js
+++ script.js
@@ -149,6 +149,11 @@ document.addEventListener("DOMContentLoaded", () => {
       if (Date.now() < end) requestAnimationFrame(frame);
     })();
   }
-
+// Visa bara fredag
+  if (new Date().getDay() !== 5) {
+    quizBox.innerHTML = "<p>Kom tillbaka på fredag för veckans 5 frågor! 📅</p>";
+  } else {
+    hamtaVeckansFragor();
+  }
   
 });
